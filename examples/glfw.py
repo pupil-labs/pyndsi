55df4f5ab1405d2440ac026dc9c4d9e1b738a29d
Pablo Prietz <pablo@prietz.org>
2017-01-18 14:57:21 +0100

    Simplify glfw lib loading

--- examples/glfw.py
+++ examples/glfw.py
@@ -61,17 +61,8 @@ if getattr(sys, 'frozen', False):
 
 else:
     # we are running in a normal Python environment
-    if os_name == "Linux":
-        dll_path = find_library('glfw')
-    elif os_name == "Darwin":
-        dll_path = find_library('glfw')
-        if not dll_path:
-            dll_path = find_library('glfw3')
-            if dll_path:
-                # deprecation warning, TODO: remove with next release
-                logger.warning("Deprecation warning: Please update your homebrew glfw installation by running `brew migrate glfw`")
-    elif os_name == "Windows":
-        dll_path = find_library('glfw3') #os.path.join(os.path.dirname(os.path.abspath(os.path.curdir)), 'shared_modules', 'external', 'glfw3')
+    if os_name == "Windows":
+        dll_path = find_library('glfw3')
     else:
         dll_path = find_library('glfw')
     if not dll_path:
