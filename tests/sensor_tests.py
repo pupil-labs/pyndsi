9bf529b83c815855550b25c26356167cd3e24c13
Roman Roibu <romanroibu@gmail.com>
2019-07-30 11:57:44 +0200

    Add tests for SensorType and Sensor factory methods

---
+++ tests/sensor_tests.py
@@ -0,0 +1,32 @@
+import pytest
+from ndsi.sensor import SensorType, Sensor
+
+
+def test_supported_types():
+    sensor_types = SensorType.supported_types()
+    assert isinstance(sensor_types, set)
+    assert len(sensor_types) > 0
+
+
+def test_sensor_types_factory():
+    # valid sensor types
+    for sensor_type_expected in SensorType.supported_types():
+        sensor_type_actual = SensorType.supported_sensor_type_from_str(str(sensor_type_expected))
+        assert sensor_type_actual is not None
+        assert isinstance(sensor_type_actual, SensorType)
+        assert sensor_type_actual is sensor_type_expected
+    # invalid sensor types
+    sensor_type = SensorType.supported_sensor_type_from_str('foo')
+    assert sensor_type is None
+
+
+def test_sensor_factory():
+    for sensor_type in SensorType.supported_types():
+        sensor_class = Sensor.class_for_type(sensor_type=sensor_type)
+        assert issubclass(sensor_class, Sensor)
+
+
+if __name__ == "__main__":
+    test_supported_types()
+    test_sensor_types_factory()
+    test_sensor_factory()
