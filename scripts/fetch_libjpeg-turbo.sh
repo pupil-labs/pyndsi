2ea6cbf3a3d199afc639bc5d0da3d9f23b40d840
Pablo Prietz <pablo@prietz.org>
2021-10-11 17:06:33 +0200

    Use curl instead of wget to avoid ssl cert validation issues

--- scripts/fetch_libjpeg-turbo.sh
+++ scripts/fetch_libjpeg-turbo.sh
@@ -1,4 +1,4 @@
-wget -q -O libjpeg-turbo.tar.gz "https://sourceforge.net/projects/libjpeg-turbo/files/1.5.1/libjpeg-turbo-1.5.1.tar.gz/download"
+curl -sL -o libjpeg-turbo.tar.gz "https://sourceforge.net/projects/libjpeg-turbo/files/1.5.1/libjpeg-turbo-1.5.1.tar.gz/download"
 tar xzf libjpeg-turbo.tar.gz
 mv libjpeg-turbo-* libjpeg-turbo-build
 cd libjpeg-turbo-build
