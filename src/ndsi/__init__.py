f041bddf4900b193db1b413e619b62031cc38995
Pablo Prietz <pablo@prietz.org>
2021-10-15 18:31:12 +0200

    Bump version: 1.4.2 → 1.4.3

--- src/ndsi/__init__.py
+++ src/ndsi/__init__.py
@@ -23,7 +23,7 @@ class StreamError(CaptureError):
 
 from ndsi.formatter import DataFormat
 
-__version__ = "1.4.2"
+__version__ = "1.4.3"
 __protocol_version__ = str(DataFormat.latest().version_major)
 
 
